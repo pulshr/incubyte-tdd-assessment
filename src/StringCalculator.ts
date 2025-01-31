08c92d28336a6293a7797ff11bf75d3dc0cc9439
Pulkit <pulkit.shrivastava@gravityer.com>
2025-01-31 18:34:40 +0530

    Verified handling of multiple negative numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,22 +1,17 @@
 export class StringCalculator {
   static add(numbers: string): number {
     if (!numbers) return 0;
-
     let delimiter = /,|\n/;
-
     if (numbers.startsWith("//")) {
       const parts = numbers.split("\n");
       delimiter = new RegExp(parts[0].slice(2));
       numbers = parts[1];
     }
-
     const numList = numbers.split(delimiter).map(Number);
     const negatives = numList.filter((num) => num < 0);
-
     if (negatives.length) {
       throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
     }
-
     return numList.reduce((sum, num) => sum + num, 0);
   }
 }
