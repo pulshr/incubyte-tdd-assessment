34b2989f680d839c9bc62de250f350ae5edac3f9
Pulkit <pulkit.shrivastava@gravityer.com>
2025-01-31 18:23:09 +0530

    Updated add() to handle newline as a delimiter

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,7 +2,7 @@ export class StringCalculator {
   static add(numbers: string): number {
     if (!numbers) return 0;
     return numbers
-      .split(",")
+      .split(/,|\n/)
       .map(Number)
       .reduce((sum, num) => sum + num, 0);
   }
