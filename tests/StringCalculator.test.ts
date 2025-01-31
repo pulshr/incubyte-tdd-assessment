d7fe4f7d9d54d1684113fca1f81eacdce287430b
Pulkit <pulkit.shrivastava@gravityer.com>
2025-01-31 18:16:50 +0530

    Added failing test for multiple numbers

--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -13,3 +13,7 @@ test("returns the number itself for a single number", () => {
 test("returns the sum of two numbers", () => {
   expect(StringCalculator.add("1,2")).toBe(3);
 });
+
+test("returns sum for multiple numbers", () => {
+  expect(StringCalculator.add("1,2,3,4")).toBe(10);
+});
