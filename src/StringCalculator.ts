75cf5a10f03e2f6a460906bd320b0c4c082ff5c6
Pulkit <pulkit.shrivastava@gravityer.com>
2025-02-11 15:43:17 +0530

    Add feature to handle *

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,16 +1,20 @@
 export class StringCalculator {
-  static add(numbers: string): number {
+  static calculate(numbers: string): number {
     if (!numbers) return 0;
     let delimiter = /,|\n/;
     if (numbers.startsWith("//")) {
       const parts = numbers.split("\n");
-      delimiter = new RegExp(parts[0].slice(2));
+      delimiter = new RegExp(parts[0].slice(2).replace(/[*]/g, "\\$&"));
       numbers = parts[1];
     }
     const numList = numbers.split(delimiter).map(Number);
     const negatives = numList.filter((num) => num < 0);
     if (negatives.length) {
       throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+    console.log(delimiter.source);
+    if (delimiter.source === "\\*") {
+      return numList.reduce((prod, num) => prod * num, 1);
     }
     return numList.reduce((sum, num) => sum + num, 0);
   }
--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -2,38 +2,43 @@ import { StringCalculator } from "../src/StringCalculator";
 
 describe("StringCalculator", () => {
   test("returns 0 for an empty string", () => {
-    expect(StringCalculator.add("")).toBe(0);
+    expect(StringCalculator.calculate("")).toBe(0);
   });
 });
 
 test("returns the number itself for a single number", () => {
-  expect(StringCalculator.add("1")).toBe(1);
+  expect(StringCalculator.calculate("1")).toBe(1);
 });
 
 test("returns the sum of two numbers", () => {
-  expect(StringCalculator.add("1,2")).toBe(3);
+  expect(StringCalculator.calculate("1,2")).toBe(3);
 });
 
 test("returns sum for multiple numbers", () => {
-  expect(StringCalculator.add("1,2,3,4")).toBe(10);
+  expect(StringCalculator.calculate("1,2,3,4")).toBe(10);
 });
 
 test("handles new line as a delimiter", () => {
-  expect(StringCalculator.add("1\n2,3")).toBe(6);
+  expect(StringCalculator.calculate("1\n2,3")).toBe(6);
 });
 
 test("handles custom delimiter", () => {
-  expect(StringCalculator.add("//;\n1;2")).toBe(3);
+  expect(StringCalculator.calculate("//;\n1;2")).toBe(3);
 });
 
 test("throws an exception for negative numbers", () => {
-  expect(() => StringCalculator.add("1,-2,3")).toThrow(
+  expect(() => StringCalculator.calculate("1,-2,3")).toThrow(
     "negative numbers not allowed -2"
   );
 });
 
 test("throws an exception for multiple negative numbers", () => {
-  expect(() => StringCalculator.add("1,-2,-3,4")).toThrow(
+  expect(() => StringCalculator.calculate("1,-2,-3,4")).toThrow(
     "negative numbers not allowed -2,-3"
   );
+});
+
+test("returns product of numbers when custom delimiter is an asterisk", () => {
+  const result = StringCalculator.calculate("//*\n2*3");
+  expect(result).toBe(6);
 });
